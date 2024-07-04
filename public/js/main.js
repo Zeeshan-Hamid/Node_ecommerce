05ea51d047acf23313f4e9920563db10104b76f6
Zeeshan Hamid <zeeshanhamid17@gmail.com>
2024-07-04 16:53:41 +0500

    Addtiotnal Styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,6 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
-
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
+function toggleMenu() {
+  const navbarMenu = document.querySelector(".navbar-menu");
+  const navbarBackdrop = document.querySelector(".navbar-backdrop");
+  navbarMenu.classList.toggle("active");
+  navbarBackdrop.classList.toggle("active");
 }
-
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
-
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
